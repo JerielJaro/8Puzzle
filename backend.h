9ae74df2060521aa12b906b2909dd74c31b16d27
JerielJaro <jarojeriel@gmail.com>
2016-05-03 07:21:33 +0800

    revert

--- backend.h
+++ backend.h
@@ -1,43 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <math.h>
 
-int Randomize(int board[9]);
-int PrintBoard(int board[9]);
+void Randomize(int board[9]);
+void PrintBoard(int board[9]);
 int CheckBoard(int board[9]);
-int SaveBoard(int board[9]);
-int LoadBoard(int board[9]);
+void NewGame(int board[9]);
+void SaveBoard(int board[9]);
+void LoadBoard(int board[9]);
 
-/*
-	TO DO:
-	Limit the infinite loop of randomiation.
-*/
-int Randomize(int *board){
-	int i, j=sizeof(board)/sizeof(board[0]);
-	int boardCheck[j] = {0};
-	for(i=0;i<j;i++){
+void Randomize(int board[9]){
+	int i,j;
+	int boardCheck[9] = {0};
+	PrintBoard(boardCheck);
+	for(i=0;i<9;i++){
 		while(1){
 			srand(clock());
-			board[i] = rand()%j+1;
+			board[i] = rand()%9+1;
 			if(!boardCheck[board[i]]){
 				boardCheck[board[i]] = 1;
 				break;
 			}	
 		}
 	}
-	return 1;
 }
 
-int PrintBoard(int *board){
-	int i, j= sizeof(board)/sizeof(board[0]);
-	for(i=1;i<j+1;i++){
+void PrintBoard(int board[9]){
+	int i, j;
+
+	for(i=1;i<10;i++){
 		printf("%d ", board[i-1]);
-		if(!(i%sqrt(j))){
+		if(!(i%3)){
 			printf("\n");
 		}
 	}
-	return 1;
 }
 
 int CheckBoard(int board[9]){
@@ -52,31 +48,24 @@ int CheckBoard(int board[9]){
 	return inversions;
 }
 
+void NewGame(int board[9]){
+	Randomize(board);
+	if(CheckBoard(board)%2 == 0){
+	}
+}
 
-/*
-	TO DO:
-	Make the textfile user defined instead of
-	fixed.
-*/
-int SaveBoard(int board[9]){
+void SaveBoard(int board[9]){
 	FILE * fp = fopen("saveFile.txt", "w");
 	int i;
 	for(i=0;i<9;i++){
 		fprintf(fp, "%d ", board[i]);
 	}
 	fclose(fp);
-	return 1;
 }
 
-/*
-	TO DO:
-	Make the textfile user defined instead of 
-	fixed.
-*/
-int LoadBoard(int board[9]){
+void LoadBoard(int board[9]){
 	FILE * fp = fopen("saveFile.txt", "r");
 	int i=0;
 	while(fscanf(fp, "%d ", &(board[i++])) != EOF){};
 	fclose(fp);
-	return 1;
 }
