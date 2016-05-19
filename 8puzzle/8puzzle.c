ea57dfce2e36a4cf5aaa388a04d4bbe5b5b70fee
milestrong <milesbasnillo@gmail.com>
2016-05-19 15:37:38 +0800

    Finished working backend. Project completed

--- 8puzzle/8puzzle.c
+++ 8puzzle/8puzzle.c
@@ -11,42 +11,49 @@
 #include "../../sdk/dexsdk.h"
 #include "../../sdk/time.h"
 
-void Erase();
-void EraseAll();
-void PrintInstructions(int x, int y);
-void RandomizeBoard();
-void PrintMenu();
-void PrintBoard(int x, int y);						
-void PrintBorder(); 
-void PrintArrow(int x, int y);								
-void MovePiece(char d);
+//Macros used as dimensions of the 8 Puzzle board
+#define ROW 3
+#define COL 3
+#define DIM ROW*COL
 
-#define col 3
-#define row 3
+//Keys used in in-game proper of the 8 Puzzle
+#define UP 'w'
+#define UP2 (char)151
+#define DOWN 's'
+#define DOWN2 (char)152
+#define LEFT 'a'
+#define LEFT2 (char)150
+#define RIGHT 'd'
+#define RIGHT2 (char)153
+#define QUIT 'q'
+#define START 'e'
+#define YES 'y'
+#define NO 'n'
+#define ENTER '\n'
+#define SPACE ' '
 
-#define up 'w'
-#define up2 (char)151
-#define down 's'
-#define down2 (char)152
-#define left 'a'
-#define left2 (char)150
-#define right 'd'
-#define right2 (char)153
-#define quit 'q'
-#define start 'e'
-#define yes 'y'
-#define no 'n'
-#define enter '\n'
-#define space ' '
+int board[ROW][COL];
 
-/* Global variables */
-int board[row][col];
+void 	Erase();
+void 	EraseAll();
+void 	PrintBorder(); 
+void 	PrintMenu();
+void 	PrintArrow(int x, int y);	
+void 	PrintInstructions(int x, int y);
+void	NewGame(int board[ROW][COL]);
+void 	PrintBoard(int x, int y);	
+void 	ShowHint();
+//void	MovePiece(char d, int board[ROW][COL]);
+void 	MovePiece(char d);
+void 	RandomizeBoard(int board[ROW][COL]);
+int 	CheckBoard(int board[ROW][COL]);
+int		CheckWin(int board[ROW][COL]);
 
 void main(int argc, char *argv[]){
-	char keypress = start;
+	char keypress = START;
 	int ch=0;
-
-	RandomizeBoard();
+	int moves=0;
+	char mc[10];
 
 	set_graphics(VGA_320X200X256);
 
@@ -57,56 +64,60 @@ void main(int argc, char *argv[]){
 		PrintMenu();
 		keypress = (char)getch();
 
-		if(keypress == quit){
-			keypress = start;					//special condition so that it will not exit on Q press at menu
-		}else if(keypress == up || keypress == up2){
+		if(keypress == QUIT){
+			keypress = START;					//special condition so that it will not exit on Q press at menu
+		}else if(keypress == UP || keypress == UP2){
 			Erase(80, 120, 15, 15);
 			PrintArrow(80, 105);
 			ch = 0;
-		}else if(keypress == down || keypress == down2){
+		}else if(keypress == DOWN || keypress == DOWN2){
 			Erase(80, 100, 15, 15);
 			PrintArrow(80, 125);
 			ch = 1;
-		}else if(keypress == enter || keypress == space){
+		}else if(keypress == ENTER || keypress == SPACE){
 			switch(ch){
 				case 0:
 						EraseAll();
+						NewGame(board);
 						do{	//Inside-the-game loop
 							PrintInstructions(180, 30);
 							PrintBoard(30, 30);										//* Always prints update version of board at coordinates
 							PrintBorder();														//* Always prints border
+							sprintf(mc, "%d", moves);
+							Erase(55, 175, 150, 15);
+							write_text("Moves: ", 55, 175, WHITE, 0);
+							write_text(mc, 120, 175, WHITE, 0);
+							ShowHint();
+
 							keypress=(char)getch();
-							
-							if(keypress == up || keypress == up2){
-								Erase(55, 175, 150, 15);
-								//write_text("I pressed UP!", 55, 175, WHITE, 0);			//* Must update the values of the board after each keypress
-								Erase(15, 15, 150, 170);
+
+							if(keypress == UP || keypress == UP2){
+								//Erase(15, 15, 150, 150);
 								MovePiece(keypress);
-							}else if(keypress == left || keypress == left2){
-								Erase(55, 175, 150, 15);
-								//write_text("I pressed LEFT!", 55, 175, WHITE, 0);
-								Erase(15, 15, 150, 170);
+								moves++;
+							}else if(keypress == LEFT || keypress == LEFT2){
+								//Erase(15, 15, 150, 150);
 								MovePiece(keypress);
-							}else if(keypress == right || keypress == right2){
-								Erase(55, 175, 150, 15);
-								//write_text("I pressed RIGHT!", 55, 175, WHITE, 0);
-								Erase(15, 15, 150, 170);
+								moves++;
+							}else if(keypress == RIGHT || keypress == RIGHT2){
+								//Erase(15, 15, 150, 150);
 								MovePiece(keypress);
-							}else if(keypress == down || keypress == down2){
-								Erase(55, 175, 150, 15);
-								//write_text("I pressed DOWN!", 55, 175, WHITE, 0);
-								Erase(15, 15, 150, 170);
+								moves++;
+							}else if(keypress == DOWN || keypress == DOWN2){
+								//Erase(15, 15, 150, 150);
 								MovePiece(keypress);
-							}else if(keypress == quit){
+								moves++;
+							}else if(keypress == QUIT){
 								Erase(55, 175, 150, 15);
-								write_text("Are you sure? Y/N", 55, 175, WHITE, 0);
+								write_text("Really exit? Y/N", 55, 175, WHITE, 0);
 								do{
 									keypress=(char)getch();
-									if(keypress == yes){
-										keypress = quit;
+									if(keypress == YES){
+										moves = 0;
+										keypress = QUIT;
 										break;
-									}else if(keypress == no){
-										keypress = yes;
+									}else if(keypress == NO){
+										keypress = YES;
 										Erase(55, 175, 150, 15);
 										break;
 									}
@@ -115,36 +126,43 @@ void main(int argc, char *argv[]){
 								Erase(55, 175, 150, 15);
 								write_text("Invalid button!", 55, 175, WHITE, 0);
 							}
-							
-						}while(keypress != quit);
+							if(CheckWin(board)){
+								EraseAll();
+								write_text("You win!", 125, 85, WHITE, 0);
+								moves = 0;
+								keypress = QUIT;
+							}
+						}while(keypress != QUIT);
 						EraseAll();
 						PrintArrow(80, 105);
-						keypress = start;
+						keypress = START;
 						break;
-				case 1: keypress = quit;
+				case 1: keypress = QUIT;
 						break;
 				default: break;
 			}
 		}
 		
-	}while(keypress != quit);
+	}while(keypress != QUIT);
 	
 	set_graphics(VGA_TEXT80X25X16);
 	clrscr();
 }
 
-void PrintMenu(){
-	int i, y=55, color;
-	color = RED;
+void Erase(int x, int y, int w, int h){ //basically covers an area with a black rectangle 
+   int i,j;
+   for (i=y;i<=(y+h);i++)
+      for (j=x;j<=(x+w);j++)
+         write_pixel(j,i,100);
+}
 
-	write_text("8-", 20, 20, GREEN, 1);
-	write_text("Puzzle", 40, 20, RED, 1);
-	write_text("A game by CMSC 125 Students", 55, 35, WHITE, 0);
-	for(i=15;i<305;i++) write_pixel(i, y, color);
-	write_text("Start Game", 100, 100, WHITE, 1);
-	write_text("Quit", 100, 120, WHITE, 1);
-
-	write_text("ICS-OS", 250, 160, WHITE, 1);
+void EraseAll(){
+	int i, j;
+	for(i=1;i<320;i++){
+		for(j=1;j<200;j++){
+			write_pixel(i,j,100);
+		}
+	}
 }
 
 void PrintBorder(){
@@ -162,17 +180,97 @@ void PrintBorder(){
 	for(i=5;i<195;i++) write_pixel(x2+2, i, color1);	//RIGHT
 }
 
-/*
+void PrintMenu(){
+	int i, y=55, color;
+	color = RED;
 
-*/
+	write_text("8-", 20, 20, GREEN, 1);
+	write_text("Puzzle", 40, 20, RED, 1);
+	write_text("A game by CMSC 125 Students", 55, 35, WHITE, 0);
+	for(i=15;i<305;i++) write_pixel(i, y, color);
+	write_text("Start Game", 100, 100, WHITE, 1);
+	write_text("Quit", 100, 120, WHITE, 1);
+
+	write_text("ICS-OS", 250, 160, WHITE, 1);
+}
+
+void PrintArrow(int x, int y){
+	write_text(">>", x, y, WHITE, 0);
+}
+
+void PrintInstructions(int x, int y){
+	write_text("8-Puzzle Game", x, y, GREEN, 0);
+	write_text("Instructions: ", x, y+20, RED, 0);
+	write_text("W - Move Up", x, y+40, WHITE, 0);
+	write_text("S - Move Down", x, y+50, WHITE, 0);
+	write_text("A - Move Left", x, y+60, WHITE, 0);
+	write_text("D - Move Right", x, y+70, WHITE, 0);
+	write_text("Q - Exit", x, y+80, WHITE, 0);
+}
+
+void ShowHint(){
+	Erase(160, 140, 145, 45);
+	srand(time());
+	int i;
+	i = rand()%4;
+	switch(i){
+		case 0:	
+				write_text("HINT!", 160, 140, WHITE, 0);
+				write_text("Press up", 170, 160, WHITE, 0);
+				write_text("to move up!", 175, 175, WHITE, 0);
+				break;
+		case 1:
+				write_text("HINT!", 160, 140, WHITE, 0);
+				write_text("Press down", 170, 160, WHITE, 0);
+				write_text("to move down!", 175, 175, WHITE, 0);
+				break;
+		case 2:
+				write_text("HINT!", 160, 140, WHITE, 0);
+				write_text("Press left", 170, 160, WHITE, 0);
+				write_text("to move left!", 175, 175, WHITE, 0);
+				break;
+		case 3:
+				write_text("HINT!", 160, 140, WHITE, 0);
+				write_text("Press right", 170, 160, WHITE, 0);
+				write_text("to move right!", 175, 175, WHITE, 0);
+				break;
+	}
+	
+}
+
+void NewGame(int board[ROW][COL]){
+	do{
+		RandomizeBoard(board);
+	}while(!CheckBoard);
+}
+
+int CheckBoard(int board[ROW][COL]){
+	int i,j,
+		inversions=0,
+		*p = (int *)(board[0]);
+	for(i=0;i<DIM-1;i++){
+		if(!(*(p+i)) && i++) continue;
+		for(j=i+1;j<DIM;j++){
+			if((*(p+j)) && (*(p+i) > *(p+j))){
+				inversions++;
+			}
+		}
+	}
+	switch(inversions%2){
+		case 0: return 1;
+		case 1: return 0;
+		default: return -1;
+	}
+}
+
 void PrintBoard(int x, int y){
 	int i, j, k, l;
 	k = x;
 	l = y;
 
 	char str[10];
-	for(i=0; i<row; i++, l+=30){
-		for(j=0; j<col; j++, k+=45){
+	for(i=0; i<ROW; i++, l+=30){
+		for(j=0; j<COL; j++, k+=45){
 			if(board[i][j] == 0){
 				write_text(" ", k, l, WHITE, 0);
 			}else{
@@ -198,65 +296,20 @@ void PrintBoard(int x, int y){
 	}
 }
 
-/*
-	Function from the lights out example
-*/
-void Erase(int x, int y, int w, int h){ //basically covers an area with a black rectangle 
-   int i,j;
-   for (i=y;i<=(y+h);i++)
-      for (j=x;j<=(x+w);j++)
-         write_pixel(j,i,100);
-}
-
-void EraseAll(){
-	int i, j;
-	for(i=1;i<320;i++){
-		for(j=1;j<200;j++){
-			write_pixel(i,j,100);
-		}
-	}
-}
-
-void PrintInstructions(int x, int y){
-	write_text("8-Puzzle Game", x, y, GREEN, 0);
-	write_text("Instructions: ", x, y+20, RED, 0);
-	write_text("W - Move Up", x, y+40, WHITE, 0);
-	write_text("S - Move Down", x, y+50, WHITE, 0);
-	write_text("A - Move Left", x, y+60, WHITE, 0);
-	write_text("D - Move Right", x, y+70, WHITE, 0);
-	write_text("Q - Exit", x, y+80, WHITE, 0);
-}
-
-void PrintArrow(int x, int y){
-	write_text(">>", x, y, WHITE, 0);
-}
-
-
-void RandomizeBoard(){
-	int i, j, k=0;
-	//temp hardcode
-	for(i=0;i<3;i++){
-		for(j=0;j<3;j++){
-			board[i][j] = k;
-			k++;
-		}
-	}
-	//end of temp*/
-	/*
-	int boardCheck[9] = {0};
-	for(i=0;i<3;i++){
-		for(j=0;j<3;j++){
-			while(1){
-				srand(time());
-				board[i][j] = rand()%9;
-				if(boardCheck[board[i][j]] == 0){
-					boardCheck[board[i][j]] = 1;
-					break;
-				}
+void RandomizeBoard(int board[ROW][COL]){
+	int i, 
+		*p = (int *)(board[0]), 
+		checkboard[DIM] = {0};
+	srand(time(NULL));
+	for(i=0;i<9;i++){
+		do{
+			if(!checkboard[*(p+i) = rand()%(DIM)]){
+				checkboard[*(p+i)] = 1;
+				break;
 			}
 		}
+		while(1);
 	}
-	*/
 }
 
 void MovePiece(char d){
@@ -270,38 +323,32 @@ void MovePiece(char d){
 			}
 		}
 	}
-	if(d == up || d == up2){	
+	if((x < ROW-1) && (d == UP || d == UP2)){
+		Erase(15,15,150,150);	
 		board[x][y] = board[x+1][y];
 		board[x+1][y] = 0;
 	}
-	if(d == down || d == down2){
+	if(x && (d == DOWN || d == DOWN2)){
+		Erase(15,15,150,150);
 		board[x][y] = board[x-1][y];
 		board[x-1][y] = 0;
 	}
-	if(d == right || d == right2){
+	if(y && (d == RIGHT || d == RIGHT2)){
+		Erase(15,15,150,150);
 		board[x][y] = board[x][y-1];
 		board[x][y-1] = 0;
 	}
-	if(d == left || d == left2){
+	if((y < COL-1) && (d == LEFT || d == LEFT2)){
+		Erase(15,15,150,150);
 		board[x][y] = board[x][y+1];
 		board[x][y+1] = 0;
 	}
 }
 
-
-/*
-	FUNCTION FOR CHECKING INVERSIONS (TO BE USED LATER)
-
-int CheckBoard(int board[9]){
-	int i,j,inversions = 0;
-	for(i=0;i<8;i++){
-		for(j=i+1;j<9;j++){
-			if(board[i] > board[j]){
-				inversions++;
-			}
-		}
+int CheckWin(int board[ROW][COL]){
+	int i, *p = (int *)(board[0]);
+	for(i=0;i<DIM-2;i++){
+		if(*(p+i) > *(p+i+1)) return 0;
 	}
-	return inversions;
+	return 1;
 }
-
-*/
